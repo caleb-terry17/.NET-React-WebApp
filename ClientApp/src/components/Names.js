05a267caf1f17d9cc47f4fd47d1a4b1394d13516
caleb-terry17 <k.cterry30@gmail.com>
2022-12-18 18:02:08 -0600

    added list to show names

--- ClientApp/src/components/Names.js
+++ ClientApp/src/components/Names.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 export class Names extends Component {
   constructor(props) {
     super(props);
-    this.state = { namesList: [] };
+    this.state = { 
+        namesList: [],
+    };
+    this.addName = this.addName.bind(this);
   }
 
-  addName(name) {
+  addName() {
     this.setState({
-      namesList: this.state.namesList.push(name)
+      namesList: [...this.state.namesList, this.state.namesList.length],
     });
+    console.log(this.state.namesList);
+    // event.preventDefault();
   }
 
   render() {
@@ -17,7 +22,14 @@ export class Names extends Component {
       <div>
         <h1>Names</h1>
 
-        {this.state.namesList.map((name) => <p>{name}</p>)}
+        <button
+            onClick={this.addName}
+        >
+            add name
+        </button>        
+
+        <h2>List of Names:</h2>
+        {this.state.namesList.map((name, index) => <p key={index}>{name}</p>)}
       </div>
     );
   }
